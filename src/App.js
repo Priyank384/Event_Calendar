1278bed6f1f6d184aa9a5005f8d145a4c7f37fda
Priyank384 <priyankgupta384@gmail.com>
2024-01-10 12:49:15 +0530

    Assignment Deployment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Calendar from "./Components/Calendar";
+
+function App() {
+  return (
+    <div className=" m-3 md:m-5 rounded-sm border-2 font-rob border-blue-400 bg-white text-black">
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
